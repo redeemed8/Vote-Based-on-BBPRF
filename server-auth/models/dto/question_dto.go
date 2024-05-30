8435a1917f8860f66156c04b4feae550e7b6c7e9
redeemed8 <519282587@qq.com>
2024-05-30 17:08:46 +0800

    2024.5.30

--- server-auth/models/dto/question_dto.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type QuestionDto struct {
-	Id       string   `json:"id"`
-	FatherId string   `json:"father_id"`
-	Title    string   `json:"title"`
-	Type     string   `json:"type"`
-	Options  []string `json:"options"`
-}
--- server-auth/models/dto/questionnaire_dto.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type QuestionnaireDto struct {
-	Id        string `json:"id"`
-	Title     string `json:"title"`
-	AnsNumber int    `json:"ans_number"`
-	Status    int    `json:"status"`
-}
